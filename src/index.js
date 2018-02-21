d4e968fae4722cbfa13bdc74397c28f057031f9d
Federico Pazos <federico.pazos@levinglobal.com>
2018-02-21 10:07:50 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PublishTest extends Component {
+  render() {
+    return (
+      <div>This is a publish test component.</div>
+    );
+  }
+}
+
+export default PublishTest;
